5d0e752fc3ec8de4cac1eec3e1b8baf8c55850bf
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2022-06-29 15:25:20 +0800

    Finish Input & Factorization

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,112 @@
 import 'package:flutter/material.dart';
 
+List primes = <int>[];
+
+void genPrimeTable() {
+  const int maxValue = 35000;
+  List<bool> isPrime = List<bool>.filled(maxValue, true);
+  primes.add(2);
+  for (var i = 3; i < maxValue; i += 2) {
+    if (isPrime[i]) {
+      primes.add(i);
+      for (var j = i * i; j < maxValue; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+}
+
 void main() {
+  genPrimeTable();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Prime Factorization',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePageState extends State<HomePage> {
+  
+  String inputValue = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String calculate() {
+    if (inputValue == '') {
+      return '';
+    }
+    int number = int.parse(inputValue);
+    if (number < 1 || number >= 1000000000) {
+      return 'Input Error!';
+    }
+    List<List<int>> factors = [];
+    for (int i = 0; i < primes.length; i++) {
+      if (primes[i] * primes[i] > number) {
+        break;
+      }
+      while (number % primes[i] == 0) {
+        number = number ~/ primes[i];
+        if (factors.isEmpty || factors.last[0] != primes[i]) {
+          factors.add([primes[i], 1]);
+        } else {
+          factors.last[1]++;
+        }
+      }
+    }
+    if (number != 1 || factors.isEmpty) {
+      factors.add([number, 1]);
+    }
+    String ret = '';
+    for (var f in factors) {
+      if (ret != '') {
+        ret += ' x ';
+      } else {
+        ret = '= ';
+      }
+      ret += '${f[0]}^${f[1]}';
+    }
+    return ret;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  inputValue = value;
+                });
+              },
+              maxLength: 9,
+              decoration: const InputDecoration(
+                labelText: 'Enter your number',
+                hintText: '1~999,999,999',
+              ),
+              keyboardType: TextInputType.number,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text(calculate())
           ],
-        ),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
